829b7ba163cbf3759cbe71be1fbbd077bb44ce46
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-29 15:11:17 -0400

    student statement

---
+++ people.rb
@@ -0,0 +1,29 @@
+class Person
+	def initialize(first_name, last_name)
+		@first_name = first_name
+		@last_name = last_name
+	end	
+	def first_name
+		@first_name
+	end
+	def last_name
+		@last_name
+	end
+end
+
+class Student < Person
+	def learn
+		puts "I get it"
+	end
+end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
+end
+
+dave = Student.new("dave", "slipp")
+
+dave.learn
+
