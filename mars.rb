67232141cab091b1aefa92e938c46594524137f6
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-30 07:22:27 -0400

    a rough start to the Mars Rover

---
+++ mars.rb
@@ -0,0 +1,42 @@
+class Rover
+	def initialize(x, x, dir)
+		@x= x
+		@y = y
+		@dir = dir
+	end	
+	def x
+		@x
+	end
+	def y
+		@y
+	end
+	def dir
+		@dir
+	end
+	def read_instruction
+	end
+	def move
+	end
+	def turn
+	end
+end
+
+class Rover_one < Rover
+	def R1_posn
+		posn = (x,y,dir)
+		puts "R1 position - #{x} #{y} #{dir}"
+	end
+end
+
+class Rover_two < Rover
+	def R2_posn
+		puts "R2 position - #{x} #{y} #{dir}"
+	end
+end
+
+R1 = Rover_one.new(0,0, "N")
+R1.R1_posn
+
+R2 = Rover_two.new(1,0,"N")
+R2.R2_posn
+
